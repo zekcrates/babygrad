538aef85f82559fb771d3ee60afab39a244d1fad
Manik verma <verma001manik@gmail.com>
2025-10-04 18:32:37 +0530

    resnet mlpresnet

--- examples/stable_mnist.py
+++ examples/stable_mnist.py
@@ -4,6 +4,34 @@ from baby import Tensor
 from baby.optim import Adam
 from baby.nn import Module, Linear, ReLU, Sequential, SoftmaxLoss, LayerNorm1d, BatchNorm1d
 from baby.data import parse_mnist
+from baby import nn 
+
+
+
+def ResidualBlock(dim, hidden_dim, norm=nn.BatchNorm1d, drop_prob=0.1):
+    fn = nn.Sequential(nn.Linear(in_features=dim, out_features=hidden_dim),
+                       norm(dim=hidden_dim), nn.ReLU(), nn.Dropout(drop_prob),
+                       nn.Linear(in_features=hidden_dim, out_features=dim),
+                       norm(dim=dim))
+    block = nn.Sequential(nn.Residual(fn), nn.ReLU())
+    return block
+
+
+def MLPResNet(dim,
+              hidden_dim=100,
+              num_blocks=3,
+              num_classes=10,
+              norm=nn.BatchNorm1d,
+              drop_prob=0.1):
+    resnet = nn.Sequential(
+        nn.Linear(in_features=dim, out_features=hidden_dim), nn.ReLU(), *[
+            ResidualBlock(dim=hidden_dim,
+                          hidden_dim=hidden_dim // 2,
+                          norm=norm,
+                          drop_prob=drop_prob) for _ in range(num_blocks)
+        ], nn.Linear(in_features=hidden_dim, out_features=num_classes))
+    return resnet
+
 
 def train_epoch(model: Module, loss_fn: Module, optimizer: Adam, X_train: np.ndarray, y_train: np.ndarray, batch_size: int):
     """
@@ -54,30 +82,30 @@ def evaluate(model: Module, X_test: np.ndarray, y_test: np.ndarray, batch_size: 
         
     return total_acc / total_count
 if __name__ == "__main__":
-    EPOCHS = 20
+    EPOCHS = 10 
     LEARNING_RATE = 0.001
     BATCH_SIZE = 128
-    NUM_STEPS_TO_RUN = 20
-    # NUM_SAMPLES = BATCH_SIZE * NUM_STEPS_TO_RUN
     INPUT_SIZE = 784
-    HIDDEN_SIZE = 100
+    HIDDEN_SIZE = 128 
     NUM_CLASSES = 10
 
     print("Loading MNIST data...")
     X_train, y_train = parse_mnist("data/train-images-idx3-ubyte.gz", "data/train-labels-idx1-ubyte.gz")
-    X_test,y_test = parse_mnist("data/t10k-images-idx3-ubyte.gz", "data/t10k-labels-idx1-ubyte.gz")
-    # print(f"Using a subset of {NUM_SAMPLES} images to run for {NUM_STEPS_TO_RUN} steps...")
-    # X_train = X_train[:NUM_SAMPLES]
-    # y_train = y_train[:NUM_SAMPLES]
+    X_test, y_test = parse_mnist("data/t10k-images-idx3-ubyte.gz", "data/t10k-labels-idx1-ubyte.gz")
     print("Data loaded.\n")
 
     np.random.seed(42)
     
-    model = Sequential(
-        Linear(INPUT_SIZE, HIDDEN_SIZE),
-        BatchNorm1d(HIDDEN_SIZE), # LayerNorm1d
-        ReLU(),
-        Linear(HIDDEN_SIZE, NUM_CLASSES)
+
+    
+    print("Building MLPResNet model...")
+    model = MLPResNet(
+        dim=INPUT_SIZE,
+        hidden_dim=HIDDEN_SIZE,
+        num_classes=NUM_CLASSES,
+        num_blocks=3,             
+        norm=nn.BatchNorm1d,       
+        drop_prob=0.1              
     )
     
     loss_fn = SoftmaxLoss()
@@ -92,4 +120,3 @@ if __name__ == "__main__":
         test_acc = evaluate(model, X_test, y_test, BATCH_SIZE)
 
         print(f"--- Epoch {epoch+1}/{EPOCHS} --- Test Accuracy = {test_acc*100:.2f}% ---")
-        print("-" * 20)
