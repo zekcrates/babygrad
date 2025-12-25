9357585b7a9412393d4f0e7592ad1e2b6e13a756
Manik verma <verma001manik@gmail.com>
2025-12-25 17:25:03 +0530

    added nn.conv

--- baby/nn.py
+++ baby/nn.py
@@ -1,3 +1,4 @@
+import math
 from typing import Any
 from baby import init, ops
 from .tensor import Tensor
@@ -483,4 +484,51 @@ class Embedding(Module):
         embedded_data = self.weight.data[x.data.astype(int)]
         return Tensor(embedded_data)
 
+
+
+
+
+class Conv(Module):
+    """
+    Multi-channel 2D convolutional layer
+    IMPORTANT: Accepts inputs in NCHW format, outputs also in NCHW format
+    Only supports padding=same
+    No grouped convolution or dilation
+    Only supports square kernels
+    """
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, bias=True, device=None, dtype="float32"):
+        super().__init__()
+        if isinstance(kernel_size, tuple):
+            kernel_size = kernel_size[0]
+        if isinstance(stride, tuple):
+            stride = stride[0]
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.weight = Parameter(init.kaiming_uniform(
+            fan_in=in_channels * kernel_size * kernel_size, 
+            fan_out=out_channels * kernel_size * kernel_size, 
+            shape=(self.kernel_size, self.kernel_size, self.in_channels, self.out_channels), 
+            device=device, 
+            dtype=dtype
+        ))        
+        if bias:
+            fan_in = in_channels * kernel_size * kernel_size
+            bound = 1.0 / math.sqrt(fan_in)
+            self.bias = Parameter(init.rand(out_channels, low=-bound, high=bound, device=device, dtype=dtype))
+        else:
+            self.bias = None
+        self.padding = (self.kernel_size - 1) // 2
+        
+    def forward(self, x: Tensor) -> Tensor:
+        # input: NCHW -> NHWC
+        x = x.transpose((1, 2)).transpose((2, 3))
+        x = ops.conv(x, self.weight, self.stride, self.padding)
+        if self.bias is not None:
+            bias = ops.reshape(self.bias, (1, 1, 1, self.out_channels))
+            bias = ops.broadcast_to(bias, x.shape)
+            x = x + bias
+        return x.transpose((2, 3)).transpose((1, 2))
+        
 
